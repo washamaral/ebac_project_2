1e5f07aa8af3f367c129bf96385eb21f7d5cc970
Wash Amaral <washmrl.silva@gmail.com>
2024-02-21 07:20:50 -0300

    starting js

---
+++ main.js
@@ -0,0 +1,43 @@
+const form = document.getElementById('form-contatos')
+const nomes = []
+const telefones = []
+
+let linhas = ''
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    
+    adicionaLinha()
+    //atualizaTabela()
+})
+
+
+function adicionaLinha() {
+    const inputContato = documentt.getElementById('contact-name')
+    const inputTelefone = document.getElementById('contact-number')
+
+    if (nomes.includes(inputContato.value)) {
+        alert(`O contato ${inputContato.value} já existe.`)
+
+    } else {
+        nomes.push(inputContato.value)
+        //telefones.push(inputTelefone.value)
+
+        let linha = '<tr>'
+        linha += `<td>${inputContato.value}</td>`
+        linha += `<td>${inputTelefone.value}</td>`
+        linha += '</tr>'
+
+        const corpoTabela = document.getElementById('tbody')
+        corpoTabela.innerHTML = linha
+      //  linhas += linha
+    }
+}
+
+inputContato.value = ''
+inputTelefone.value = ''
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody')
+    corpoTabela.innerHTML = linhas
+}
